7b03047ada605473ddc4e8ab2e2f6dc25e3e35f3
Garrett Johnson <itwinx@yahoo.com>
2017-04-02 22:33:35 +0000

    Added date and time response to index in views.

--- helloworld/views.py
+++ helloworld/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import datetime
 
 # Create your views here.
 
 def index(request):
-    return HttpResponse('This is the hello world index. Sup bruh.')
+    time = datetime.datetime.now()
+    return HttpResponse('The current date and time are %s.' % time)
